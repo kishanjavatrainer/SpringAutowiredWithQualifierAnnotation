ac96204f6e313bcd4bbd4455da1ad6d68fe96069
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-03-07 23:04:56 +0530

    SpringAutowiredWithQualifierAnnotation Source code

---
+++ SpringAutowiredWithQualifierAnnotation/src/com/infotech/model/Employee.java
@@ -0,0 +1,34 @@
+package com.infotech.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Employee {
+
+	private int employeeId;
+	private String employeeName;
+	
+	@Autowired
+	@Qualifier("pancard2")
+	private Pancard pancard;
+
+	public Pancard getPancard() {
+		return pancard;
+	}
+	public void setPancard(Pancard pancard) {
+		this.pancard = pancard;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+}
